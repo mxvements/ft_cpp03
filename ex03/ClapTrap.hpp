4b35092e80b61cd7d57f018564469972f9bb2f86
luciama2 <luciama2@student.42madrid.com>
2024-10-19 19:13:49 +0200

    started ex03

---
+++ ex03/ClapTrap.hpp
@@ -0,0 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: luciama2 <luciama2@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/16 20:52:02 by luciama2          #+#    #+#             */
+/*   Updated: 2024/10/19 18:43:52 by luciama2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_HPP
+#define CLAPTRAP_HPP
+
+#include <string>
+#include <iostream>
+#include <iomanip>
+
+// Color font code
+# define BLACK "\x1B[30m"
+# define RED "\x1b[31m"
+# define GREEN "\x1b[32m"
+# define YELLOW "\x1b[33m"
+# define BLUE "\x1b[34m"
+# define MAGENTA "\x1b[35m"
+# define CYAN "\x1b[36m"
+# define WHITE "\x1B[37m"
+# define ORANGE "\x1B[38;2;255;128;0m"
+# define ROSE "\x1B[38;2;255;151;203m"
+# define LBLUE "\x1B[38;2;53;149;240m"
+# define LGREEN "\x1B[38;2;17;245;120m"
+# define GRAY "\x1B[38;2;176;174;174m"
+# define RESET "\x1b[0m"
+// Color background code
+# define BG_BLACK "\x1B[40m"
+# define BG_RED "\x1B[41m"
+# define BG_GREEN "\x1B[42m"
+# define BG_YELLOW "\x1B[43m"
+# define BG_BLUE "\x1B[44m"
+# define BG_MAGENTA "\x1B[45m"
+# define BG_CYAN "\x1B[46m"
+# define BG_WHITE "\x1B[47m"
+# define BG_ORANGE "\x1B[48;2;255;128;0m"
+# define BG_LBLUE "\x1B[48;2;53;149;240m"
+# define BG_LGREEN "\x1B[48;2;17;245;120m"
+# define BG_GRAY "\x1B[48;2;176;174;174m"
+# define BG_ROSE "\x1B[48;2;255;151;203m"
+
+class ClapTrap
+{
+	protected:
+		std::string		_name;
+		unsigned int	_hit_pts; // = 10; health
+		unsigned int	_energy_pts; // = 10; mana, ability to attack or be repaired
+		unsigned int	_attack_damage; // = 0; amount of damage
+	
+	public:
+		ClapTrap(void);
+		ClapTrap(std::string name);
+		~ClapTrap(void);
+		ClapTrap(const ClapTrap &claptrap);
+		ClapTrap &operator=(const ClapTrap &claptrap);
+		//setters and getters
+		void setName(std::string name);
+		void setHitPts(unsigned int amount);
+		void setEnergyPts(unsigned int amount);
+		void setAttackDamage(unsigned int amount);
+		std::string getName(void);
+		int getHitPts(void);
+		int getEnergyPts(void);
+		int getAttackDamage(void);
+		std::string getClassName(void);
+		//member functs
+		void attack(const std::string &target);
+		void takeDamage(unsigned int amount);
+		void beRepaired(unsigned int amount);
+		virtual	void status(void);
+} ;
+
+#endif 
---
+++ ex03/FragTrap.cpp
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   FragTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: luciama2 <luciama2@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/19 18:02:09 by luciama2          #+#    #+#             */
+/*   Updated: 2024/10/19 18:21:58 by luciama2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "FragTrap.hpp"
+
+FragTrap::FragTrap(void)
+{
+	std::cout << this->getClassName() << ": Constructor called" << std::endl;
+}
+
+FragTrap::FragTrap(std::string name): ClapTrap(name)
+{
+	std::cout << this->getClassName() << ": Constructor called" << std::endl;
+	this->_hit_pts = 100;
+	this->_energy_pts = 100;
+	this->_attack_damage = 30;
+}
+
+FragTrap::~FragTrap(void)
+{
+	std::cout << this->getClassName() << ": Destructor called" << std::endl;
+}
+
+FragTrap::FragTrap(const FragTrap &fragtrap)
+{
+	std::cout << this->getClassName() << ": Copy constructor called" << std::endl;
+	*this  = fragtrap;
+}
+
+FragTrap &FragTrap::operator=(const FragTrap &fragtrap)
+{
+	std::cout << this->getClassName() << ": Assignment overload called" << std::endl;
+	if (this != &fragtrap)
+	{
+		this->_name = fragtrap._name;
+		this->_hit_pts = fragtrap._hit_pts;
+		this->_energy_pts = fragtrap._energy_pts;
+		this->_attack_damage = fragtrap._attack_damage;
+	}
+	return *this;
+}
+
+std::string FragTrap::getClassName(void)
+{
+	return ("FragTrap");	
+}
+
+void FragTrap::highFivesGuys(void)
+{
+	std::cout << YELLOW << "High fives request!" << RESET << std::endl;
+}
