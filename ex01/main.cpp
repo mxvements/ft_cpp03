c5210e6664241c31f1ee4f0570ffac439fc606dc
luciama2 <luciama2@student.42madrid.com>
2024-10-19 19:06:14 +0200

    fix(ex02): added tests to main

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: luciama2 <luciama2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 20:51:37 by luciama2          #+#    #+#             */
-/*   Updated: 2024/10/19 17:18:28 by luciama2         ###   ########.fr       */
+/*   Updated: 2024/10/19 19:04:34 by luciama2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,30 +15,25 @@
 
 int	main(void)
 {
-	ClapTrap claptrap("cp-robot");
 	std::string target = "target robot";
-
-	claptrap.status();
-	claptrap.attack(target);
-	claptrap.status();
-	claptrap.takeDamage(5);
-	claptrap.status();
-	claptrap.takeDamage(6);
-	claptrap.status();
-	claptrap.beRepaired(2);
-	claptrap.status();
-
+	
 	ScavTrap scavtrap("st-robot");
-
 	scavtrap.status();
 	scavtrap.attack(target);
-	scavtrap.status();
 	scavtrap.takeDamage(5);
-	scavtrap.status();
-	scavtrap.takeDamage(6);
-	scavtrap.status();
 	scavtrap.beRepaired(2);
 	scavtrap.status();
-	
+
+	ScavTrap st_assing;
+	st_assing = scavtrap;
+	st_assing.status();
+	st_assing.takeDamage(7);
+	st_assing.guardGate();
+	st_assing.beRepaired(3);
+	st_assing.status();
+
+	ScavTrap st_copy(st_assing);
+	st_copy.status();
+
 	return (0);
 }
